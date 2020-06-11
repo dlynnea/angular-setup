6c2cfc3e22edff6cc5cfc521a2c5b940c8581028
dlynnea <danimatton@gmail.com>
2020-06-10 20:01:28 -0600

    added a warning component

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning',
   templateUrl: './warning.component.html',
   styleUrls: ['./warning.component.css']
 })
-export class WarningComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class WarningComponent {
 
 }
