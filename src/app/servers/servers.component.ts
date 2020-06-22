5d400e3a3f9c7dc443641b5cbfc65cb60805c34d
dlynnea <danimatton@gmail.com>
2020-06-22 17:36:34 -0600

    using directive ngFor to loop through and add to list of servers

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
     serverCreationStatus = 'No server was created';
     serverName = 'Test server';
     serverCreated = false;
+    servers = ['Testserver', 'Testserver2'];
 
     constructor() {
         setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
 
     onCreateServer() {
         this.serverCreated = true;
+        this.servers.push(this.serverName);
         this.serverCreationStatus = 'Server was created! Name: ' + this.serverName;
     }
 
