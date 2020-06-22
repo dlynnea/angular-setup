69af088bfe0d405da03c07be6405ea48139c3aa4
dlynnea <danimatton@gmail.com>
2020-06-22 14:27:53 -0600

    using two-way biding with FormsModule

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
     allowNewServer = false;
-    serverCreationStatus = 'No server was created'
+    serverCreationStatus = 'No server was created';
+    serverName = '';
 
     constructor() {
         setTimeout(() => {
@@ -21,6 +22,10 @@ export class ServersComponent implements OnInit {
     }
 
     onCreateServer() {
-        this.serverCreationStatus = 'Server was created!';
+        this.serverCreationStatus = 'Server was created! Name: ' + this.serverName;
+    }
+
+    onUpdateServerName(event: any) {
+        this.serverName = (<HTMLInputElement>event.target).value;
     }
 }
