0540af0899711eaf390c29510b99a0d9499ec48b
dlynnea <danimatton@gmail.com>
2020-06-22 23:07:31 -0600

    using directive ngFor to loop through and add to list of clicks on toggle

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -14,7 +14,8 @@ export class UsernameComponent {
 
     onToggleCount() {
         this.showSecret = !this.showSecret;
-        this.log.push(this.log.length + 1);
+        // this.log.push(this.log.length + 1);
+        this.log.push(new Date())
     }
 
     onCreateUser() {
